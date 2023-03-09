3c987888f0ca1780add6d692c63676f750cf338b
Riizv <kchojnowski12@gmail.com>
2023-03-09 19:33:16 +0100

    Add Getting Local IPAddr

--- info.go
+++ info.go
@@ -3,9 +3,29 @@ package main
 import (
 	"fmt"
 	"time"
+	"runtime"
+	"net"
+	"log"
 )
 
+func GetOutboundIP() net.IP {
+	conn, err := net.Dial("udp", "8.8.8.8:80")
+	if err!= nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+
+	localAddr := conn.LocalAddr().(*net.UDPAddr)
+
+	return localAddr.IP
+}
+
 func main(){
-	fmt.Print("Hello world")
-	time.Sleep(time.Second*2)
+	fmt.Println("Informations about you: ")
+	fmt.Println("Operating system: " + runtime.GOOS)
+	fmt.Println("Architecture: " + runtime.GOARCH)
+	fmt.Println("IP: " + GetOutboundIP().String())
+	time.Sleep(3 * time.Second)
+
+
 }
