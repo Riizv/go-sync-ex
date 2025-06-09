c7495ec4e3f0bb4e973012a210c9255b450434f1
Riizv <kchojnowski12@gmail.com>
2025-06-09 23:47:20 +0200

    TODO server

--- internal/server/http.go
+++ internal/server/http.go
@@ -1,35 +1,36 @@
 package server
 
 import (
-    "encoding/json"
-    "fmt"
-    "net/http"
-    "time"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
 
-    "example.com/sysinfo/internal/info"
+	"example.com/sysinfo/internal/info"
 )
 
+//TODO: Make server simpler
 // New zwraca wstępnie skonfigurowany http.Server
 func New(addr string, sys *info.Info) *http.Server {
-    mux := http.NewServeMux()
+	mux := http.NewServeMux()
 
-    // Root – prosty tekst
-    mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
-    })
+	// Root – prosty tekst
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+	})
 
-    // /api/info – JSON
-    mux.HandleFunc("/api/info", func(w http.ResponseWriter, r *http.Request) {
-        js, _ := json.MarshalIndent(sys, "", "  ")
-        w.Header().Set("Content-Type", "application/json")
-        w.Write(js)
-    })
+	// /api/info – JSON
+	mux.HandleFunc("/api/info", func(w http.ResponseWriter, r *http.Request) {
+		js, _ := json.MarshalIndent(sys, "", "  ")
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(js)
+	})
 
-    return &http.Server{
-        Addr:         addr,
-        Handler:      mux,
-        ReadTimeout:  5 * time.Second,
-        WriteTimeout: 10 * time.Second,
-        IdleTimeout:  120 * time.Second,
-    }
+	return &http.Server{
+		Addr:         addr,
+		Handler:      mux,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		IdleTimeout:  120 * time.Second,
+	}
 }
